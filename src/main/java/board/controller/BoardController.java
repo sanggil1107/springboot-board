38400caec4e61fa065a23df30569258abf8e8d94
sanggil1107 <yangpang1107@gmail.com>
2021-05-18 23:37:40 +0900

    controller, view 설정

--- src/main/java/board/controller/BoardController.java
+++ src/main/java/board/controller/BoardController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import board.dto.BoardDto;
@@ -17,12 +21,23 @@ public class BoardController {
 
   @RequestMapping("/board/BoardList.do")
   public ModelAndView BoardList() throws Exception {
-    ModelAndView mv = new ModelAndView("/board/boardList");
+    ModelAndView mv = new ModelAndView();
 
     List<BoardDto> list = boardService.selectBoardList();
+    mv.setViewName("board/boardList");
     mv.addObject("list", list);
 
     return mv;
   }
 
+  @RequestMapping(value="/board/List", method=RequestMethod.GET)
+  public String List(Model model) {
+	  model.addAttribute("data", "하이");
+	  return "board/boardList";
+  }
+
+  @RequestMapping("/")
+  public String hello() {
+    return "hello";
+  }
 }
